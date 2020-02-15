993af531e5d136144bc6d01d395909ee1732d914
dani-j-d <59181296+dani-j-d@users.noreply.github.com>
2020-02-15 12:17:31 -0600

    sdsd

--- read_test_field.py
+++ read_test_field.py
@@ -1,12 +1,12 @@
 import pyodbc
-conn = pyodbc.connect('Driver=(SQL Server);'
+conn = pyodbc.connect('Driver=SQL Server;'
                       'Server=DESKTOP-MPF2K2N\TESTDB;'
                       'Database=master;'
                       'Trusted_Connection=yes;'
                       )
 
-cursor = conn.execute()
-cursor.execute('select * from master.dbo.test_field')
+cursor = conn.execute('select * from master.dbo.test_field')
+cursor.execute()
 
 for row in cursor:
     print(row)
